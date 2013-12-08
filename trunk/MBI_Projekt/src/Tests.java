677bdd55f65d16a08538f25338955e4c30689910
piotrekg499@gmail.com <piotrekg499@gmail.com@3e6e5631-0926-1e7a-5430-6e58a6aae9c5>
2013-12-08 18:55:21 +0000

    Obliczanie wektorów i wartości własnych (wywołanie jednej funkcji;-) ) plus małe poprawki

--- trunk/MBI_Projekt/src/Tests.java
+++ trunk/MBI_Projekt/src/Tests.java
@@ -5,6 +5,22 @@ public class Tests
 	public static void testLoadingMatrixFromFile(String fileName)
 	{	
 		Matrix loadedMatrix = Utils.getInputCSVData(fileName);
-		Utils.displayMatrix(loadedMatrix);
+		Utils.displayMatrix(loadedMatrix);		
+	}
+	
+	public static void testAll()
+	{
+		MatrixOperations mOp = new MatrixOperations("dane.csv");
+		Utils.displayMatrix(mOp.getMatrix());
+		double[] colsAvg = mOp.getAvgCols(mOp.getMatrix());
+		
+		for(int i=0; i<colsAvg.length;i++)
+			System.out.print(colsAvg[i]+" ");
+		
+		Utils.displayMatrix(mOp.subtractAvgValues(mOp.getMatrix(), colsAvg));
+		
+		Utils.displayMatrix(mOp.covarianceMatrix(mOp.getMatrix()));
+		
+		//Utils.displayMatrix(mOp.computePCA());
 	}
 }
