327fec700a9f2fed7249f12526b0cad9427aa273
kacper.mazur89@gmail.com <kacper.mazur89@gmail.com@3e6e5631-0926-1e7a-5430-6e58a6aae9c5>
2013-12-22 18:35:41 +0000

    dodanie opcji uruchamiania algorytmu z zadaną wyjściową liczbą atrybutów domyślnie używane było procentowe pokrycie danych wejściowaych przez nowe atrybuty

--- trunk/MBI_Projekt/src/Main.java
+++ trunk/MBI_Projekt/src/Main.java
@@ -1,57 +1,94 @@
 public class Main {
 
+	static boolean corelation = false, debug = false;
+	static int input = -2, output = -2, threshold = -2, attrPos = -2,
+			attrs = 0;
+	static String inName = null, outName = null;
+	static double thresh = 80;
+
 	// uruchamianie z konsoli (cmd):
 	// java -jar PCA.jar -i testData2.csv -o outData.csv -cor -t 81
+	/**
+	 * @param arg0
+	 */
 	public static void main(String[] arg0) {
-		boolean corelation = false, debug = false;
-		int input = -2, output = -2, threshold = -2;
-		String inName = null, outName = null;
-		double thresh = 80;
+		for (int i = 0; i < arg0.length; ++i)
+			checkAttributes(arg0, i);
 
-		String[] options = arg0;
-		for (int i = 0; i < options.length; ++i) {
-			// System.out.println(i + ": '" + options[i] + "'");
+		boolean useThresh = (attrPos == -2);
+		printAttributes(useThresh);
 
-			if (options[i].equals("-i"))
-				input = i;
-			else if (options[i].equals("-o"))
-				output = i;
-			else if (options[i].equals("-t"))
-				threshold = i;
-			else if (options[i].equals("-help"))
-				printHelp(false);
-			else if (options[i].equals("-cor"))
-				corelation = true;
-			else if (options[i].equals("-d"))
-				debug = true;
-			else if (input == (i - 1))
-				inName = options[i];
-			else if (output == (i - 1))
-				outName = options[i];
-			else if (threshold == (i - 1))
-				thresh = Double.parseDouble(options[i]);
-			else
-				printHelp(true);
-		}
+		if (canStart())
+			new MatrixOperations(inName).run(outName, corelation,
+					useThresh ? thresh : attrs, useThresh);
+		else
+			System.out.print("\nERROR in parameteres - program closed");
+	}
 
+	/**
+	 * @param useThresh
+	 */
+	private static void printAttributes(boolean useThresh) {
 		System.out.println("Parameters: ");
-		System.out.println("Input file  : " + inName);
-		System.out.println("Output file : " + outName);
-		System.out.println("Threshold   : " + thresh + "%");
-		System.out.println("Cov/Cor     : " + (corelation ? "Cor" : "Cov"));
-		System.out.println("Debug mode  : " + debug + "\n");
+		System.out.println("Input file             : " + inName);
+		System.out.println("Output file            : " + outName);
+		System.out.println("Using threshold        : " + useThresh);
+		System.out.println("Threshold              : " + thresh + "%");
+		System.out.println("Using attributes count : " + !useThresh);
+		System.out.println("Attributes             : " + attrs);
+		System.out.println("Covariance/Corelation  : "
+				+ (corelation ? "Cor" : "Cov"));
+		System.out.println("Debug mode             : " + debug + "\n");
 
 		if (debug)
 			Utils.EnableDebugMode();
 		else
 			Utils.DisableDebugMode();
-
-		// Tests.testLoadingMatrixFromFile("dane.csv");
-		// Tests.testAll();
-
-		new MatrixOperations(inName).run(outName, corelation, thresh);
 	}
 
+	/**
+	 * @param options
+	 * @param i
+	 */
+	private static void checkAttributes(String[] options, int i) {
+		if (options[i].equals("-i"))
+			input = i;
+		else if (options[i].equals("-o"))
+			output = i;
+		else if (options[i].equals("-t"))
+			threshold = i;
+		else if (options[i].equals("-a"))
+			attrPos = i;
+		else if (options[i].equals("-help"))
+			printHelp(false);
+		else if (options[i].equals("-cor"))
+			corelation = true;
+		else if (options[i].equals("-d"))
+			debug = true;
+		else if (input == (i - 1))
+			inName = options[i];
+		else if (output == (i - 1))
+			outName = options[i];
+		else if (threshold == (i - 1))
+			thresh = Double.parseDouble(options[i]);
+		else if (attrPos == (i - 1))
+			attrs = Integer.parseInt(options[i]);
+		else
+			printHelp(true);
+	}
+
+	/**
+	 * @return
+	 */
+	private static boolean canStart() {
+		return inName != null
+				&& outName != null
+				&& ((thresh >= 0 && thresh <= 100 && attrPos == -2) || (threshold == -2 && attrs != 0));
+	}
+
+	/**
+	 * @param withWaring
+	 */
 	private static void printHelp(boolean withWaring) {
 		if (withWaring)
 			System.out.println("Warning: Unrecognized option");
@@ -59,8 +96,11 @@ public class Main {
 		System.out.println("-help     - help");
 		System.out.println("-i %file% - input file name");
 		System.out.println("-o %file% - output file name:");
-		System.out.println("-d - debug mode:");
+		System.out.println("-a %attributes_count% - new attributes lenght:");
+		System.out
+				.println("-t %threshold% - percentage coverage of exising dataset by new attributes:");
 		System.out
 				.println("-cor      - using corelation matrix instead covariance:");
+		System.out.println("-d - debug mode:");
 	}
 }
